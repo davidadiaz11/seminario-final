8b523299142721e4b31ffc6bb7b7440ea443ad0d
David Díaz <davidadiaz.11@gmail.com>
2024-05-27 22:14:27 -0300

    Implementación de principios SOLID para el cálculo de alertas

---
+++ proyecto/seminario-final/Models/CalculosAlerta/CalculoPorcentual.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace seminario_final.Models
+{
+    internal class CalculoPorcentual: ICalculoAlerta
+    {
+        public List<ModelNutrienteProducto> Calcular(ModelProducto item, ModelNutrienteProducto item2)
+        {
+            List<ModelNutrienteProducto> listaAlertas = new List<ModelNutrienteProducto>();
+            var multiplicador = ServiceTiposNutriente.ObtenerTipoNutrientes(item2.Nutriente.TipoNutriente.Id).CaloriasPorGramo * 100;
+
+            var cantidadPorcentual = (item2.Nutriente.CantidadPorPorcion * multiplicador) / item.ValorEnergetico;
+
+            if (item2.Nutriente.NutrientesAlerta[0].Operador == ">" && cantidadPorcentual >= item2.Nutriente.NutrientesAlerta[0].ValorCritico)
+            {
+                item2.Nutriente.NutrientesAlerta[0].ValorReal = cantidadPorcentual;
+                item2.Nutriente.NutrientesAlerta[0].ValorDiferencial = (item2.Nutriente.NutrientesAlerta[0].ValorReal - item2.Nutriente.NutrientesAlerta[0].ValorCritico) / item2.Nutriente.NutrientesAlerta[0].ValorCritico * 100;
+                listaAlertas.Add(item2);
+            }
+            else if (item2.Nutriente.NutrientesAlerta[0].Operador == "<" && cantidadPorcentual <= item2.Nutriente.NutrientesAlerta[0].ValorCritico)
+            {
+                item2.Nutriente.NutrientesAlerta[0].ValorReal = cantidadPorcentual;
+                item2.Nutriente.NutrientesAlerta[0].ValorDiferencial = (item2.Nutriente.NutrientesAlerta[0].ValorReal - item2.Nutriente.NutrientesAlerta[0].ValorCritico) / item2.Nutriente.NutrientesAlerta[0].ValorCritico * 100;
+                listaAlertas.Add(item2);
+            }
+            return listaAlertas;
+        }
+    }
+}
