4cd0c94d8ccbcfb439ae3840cca8abd303e004ca
David Díaz <davidadiaz.11@gmail.com>
2024-06-02 19:49:14 -0300

    corrección de menú

--- proyecto/seminario-final/MasterPage.Master.designer.cs
+++ proyecto/seminario-final/MasterPage.Master.designer.cs
@@ -60,15 +60,6 @@ namespace seminario_final
         protected global::System.Web.UI.HtmlControls.HtmlAnchor A2;
 
         /// <summary>
-        /// Control A1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor A1;
-
-        /// <summary>
         /// Control Cuerpo.
         /// </summary>
         /// <remarks>
