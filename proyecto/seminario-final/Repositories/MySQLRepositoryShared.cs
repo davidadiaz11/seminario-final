5840357532114cdb127c178cb3110f7e5c26ac49
David Díaz <davidadiaz.11@gmail.com>
2024-06-07 20:16:19 -0300

    corrige cadena de conexion

--- proyecto/seminario-final/Repositories/MySQLRepositoryShared.cs
+++ proyecto/seminario-final/Repositories/MySQLRepositoryShared.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 public class MySQLRepositoryShared
 {
-    private static string cadena = Environment.GetEnvironmentVariable("CONNECTION_STRING_TESIS_TEST");
+    //private static string cadena = Environment.GetEnvironmentVariable("CONNECTION_STRING_TESIS_TEST");
+    private static string cadena = Environment.GetEnvironmentVariable("CONNECTION_STRING_TESIS_LOCAL");
     public static string getConnectionString()
     {
-        //return cadena;
-        return @"server=127.0.0.1;user id=root;database=sem-final-test-0;password=794685789;";
+        return cadena;
     }
 }
