bc88e3766a634f493dbdc77593b669537f38523c
David Díaz <davidadiaz.11@gmail.com>
2024-06-12 17:18:04 -0300

    agrega repositoryshared

---
+++ proyecto/seminario-final/Repositories/MySQLRepositoryShared.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class MySQLRepositoryShared
+{
+    //private static string cadena = Environment.GetEnvironmentVariable("CONNECTION_STRING_TESIS_TEST");
+    private static string cadena = Environment.GetEnvironmentVariable("CONNECTION_STRING_TESIS_LOCAL");
+    public static string getConnectionString()
+    {
+        return cadena;
+    }
+}
